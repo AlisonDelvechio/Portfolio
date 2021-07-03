7b7058c03a53a929475a4be6758ea6be1a7b145b
Alison Delvechio <alisondelvechio@Outlook.com>
2021-07-03 13:09:31 -0300

    Adicionando animação para os circulos, e botão main para os projetos

--- script.js
+++ script.js
@@ -1,21 +1,81 @@
+// Mouse circle
 const mouseCircle = document.querySelector('.mouse-circle')
 const mouseDot = document.querySelector('.mouse-dot')
 
-// Mouse circle
 const mouseCircleFn = (x, y) => {
     mouseCircle.style.cssText = `top:${y}px; left:${x}px; opacity: 1`;
     mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
 }
+// End of Mouse circle
+
+// Animated circles
+const circles = document.querySelectorAll('.circle')
+const mainImg = document.querySelector(".main-circle img")
+
+let mX = 0;
+let mY = 0;
+const z = 90;
+
+const animatedCircles = (e, x, y)=> {
+    if (x < mX) {
+        circles.forEach(circle => {
+            circle.style.left = `${z}px`;
+        })
+        // mainImg.style.left = `${z}px`;
+    } else if (x > mX) {
+        circles.forEach(circle => {
+            circle.style.left = `-${z}px`;
+        })
+        // mainImg.style.left = `-${z}px`;
+    }
+
+    if (y < mY) {
+        circles.forEach(circle => {
+            circle.style.top = `${z}px`;
+        })
+        // mainImg.style.top = `${z}px`;
+    } else if (y > mY) {
+        circles.forEach(circle => {
+            circle.style.top = `-${z}px`;
+        })
+        // mainImg.style.top = `-${z}px`;
+    }
+
+    mX = e.clientX;
+    mY = e.clientY;
+}
+// End of Animated circles
 
 document.body.addEventListener("mousemove", (e)=> {
     let x = e.clientX;
     let y = e.clientY;
 
     mouseCircleFn(x, y);
+    animatedCircles(e, x, y)
 })
 
 document.addEventListener('mouseleave', ()=> {
     mouseCircle.style.opacity = '0';
     mouseDot.style.opacity = '0';
 })
-// End of Mouse circle
+
+// Main button
+const mainBtn = document.querySelector('.main-btn')
+
+let ripple;
+
+mainBtn.addEventListener('mouseenter', (e)=> {
+    const left = e.clientX - e.target.getBoundingClientRect().left;
+    const top = e.clientY - e.target.getBoundingClientRect().top;
+
+    ripple = document.createElement('div')
+    ripple.classList.add('ripple');
+    ripple.style.left = `${left}px`;
+    ripple.style.top = `${top}px`;
+    mainBtn.prepend(ripple);
+})
+
+mainBtn.addEventListener('mouseleave', ()=> {
+    mainBtn.removeChild(ripple);
+})
+// End of Main button
