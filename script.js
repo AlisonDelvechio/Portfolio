353ff8736f457380194161ff20774de2e2acdcd0
Alison Delvechio <alisondelvechio@Outlook.com>
2021-07-08 21:05:09 -0300

    Validação do formulário

--- script.js
+++ script.js
@@ -260,4 +260,58 @@ setInterval(() => {
     }, 500);
 }, 3000);
 // End of Slideshow
+
+// Form validation
+const form = document.querySelector('.contact-form');
+const username = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+const messages = document.querySelectorAll('.message');
+
+const error = (input, message)=> {
+    input.nextElementSibling.classList.add('error');
+    input.nextElementSibling.textContent = message;
+}
+
+const sucess = (input)=> {
+    input.nextElementSibling.classList.remove('error');
+}
+
+const checkRequiredFields = (inputArr)=> {
+    inputArr.forEach((input)=> {
+        if (input.value.trim() === "") {
+            error(input, `${input.id} is required`);
+        }
+    });
+}
+
+const checkLength = (input, min)=> {
+    if (input.value.trim().length < min) {
+        error(input, `${input.id} must be at least ${min} characters`);
+    } else {
+        sucess(input);
+    }
+}
+
+const checkEmail = (input)=> {
+    const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (regEx.test(input.value.trim())) {
+        sucess(input);
+    } else {
+        error(input, `Email is not valid`);
+    }
+}
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    checkLength(username, 5);
+    checkLength(subject, 5);
+    checkLength(message, 10);
+    checkEmail(email);
+    checkRequiredFields([username, email, subject, message]);
+});
+// End of Form validation
 // End of Section 5
